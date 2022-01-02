f8152cd99bc37188547f54569ed8526545ba1998
Dwayneglevene <dwayneglevene@yahoo.com>
2022-01-01 20:45:31 -0500

    react bootcamp object array

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,33 +1,46 @@
-import React from 'react';
+import React, { Children } from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 
 // GLOBAL VARIABLES FOR ALL ITEMS IN THE BOOK COMPONENT
 
-const title= "Dragon Dragon !";
-const author = "Dwayne Levene";
-const image ='https://cdn.vox-cdn.com/thumbor/Sszn-potcL5sfhxoLeeHEOFty9A=/0x0:1600x959/920x613/filters:focal(670x165:926x421):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/69260607/FunFilms_DBSBroly_Landing.0.jpg';
+//first book in the list {
 
+const books = [
+
+{
+  img: 'https://images-na.ssl-images-amazon.com/images/I/61HsOY0oMbL._AC_UL254_SR254,254_.jpg',
+  title: ' I Love you to the Moon and Back',
+  author: 'Dwayne Levene',
+},{
+  img: 'https://images-na.ssl-images-amazon.com/images/I/61HsOY0oMbL._AC_UL254_SR254,254_.jpg',
+  title: 'Brooklyn way',
+  author:' Jigga man',
+}
+
+];ß
+//List of books creation
 function Booklist(){
   return( 
-    <section className="booklist">
-          <Book/>
-          <Book/>
-        
-
-    </section>
+   <section className = "booklist">
+     {books}
+   </section>
   )
 }
 
-function Book(props){
-  console.log(props);
+//Book creation
+
+//img title aurhor can be passed and destrcutor in function parameters  
+const Book = (props) => {
+  const {img,title,author,children} = props
+  //console.log(props);
   return(
     <article className = 'book'>
-        <img src = {image} />
+        <img src = {img} />
       
       <h1>{title}</h1>
-      <h4>{author.toUpperCase()}</h4>
-
+      <h4>{author}</h4>
+     
     </article>
     
   )
